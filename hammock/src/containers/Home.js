ec95086077a8ca73c9000f5803b145aa332cb54a
wheat <wheat.mai.haoqiu@foxmail.com>
2017-07-27 18:15:00 +0800

    test navigation by dispatch

--- hammock/src/containers/Home.js
+++ hammock/src/containers/Home.js
@@ -39,23 +39,16 @@ class Home extends React.Component {
     this.setTimeout(() => {},2500)
   }
   render () {
+    const { navigate, dispatch } = this.props.navigation;
+
     return (
       <ScrollView style={styles.container}>
             <Text>Tab title:{this.props.title} name:{this.props.name}</Text>
             <Text>Tab data:{this.props.data}</Text>
             <Button onPress={Actions.pop} title='Back' />
-            <Button onPress={() => { Actions.mainframe({ data: 'mainframe!' }); }} title='Switch to mainframe' /> 
-            <Button onPress={() => { Actions.tab_home({ data: 'tab_home!' }); }} title='Switch to tab_home' /> 
-            <Button onPress={() => { Actions.popular({ data: 'popular!' }); }} title='Switch to popular' /> 
-            <Button onPress={() => { Actions.home({ data: 'home!' }); }} title='Switch to home' /> 
-            <Button onPress={() => { Actions.tab_search({ data: 'tab_search!' }); }} title='Switch to tab_search with data' /> 
-            <Button onPress={() => { Actions.tab_inbox({ data: 'tab_inbox!' }); }} title='Switch to tab_inbox' /> 
-            <Button onPress={() => { Actions.notifications({ data: 'notifications!' }); }} title='Switch to notifications' /> 
-            <Button onPress={() => { Actions.messages({ data: 'messages!' }); }} title='Switch to messages' /> 
-            <Button onPress={() => { Actions.tab_personalinfo({ data: 'tab_personalinfo!' }); }} title='Switch to tab_personalinfo' /> 
-            <Button onPress={() => { Actions.error({ data: 'error!' }); }} title='Switch to error' /> 
-            <Button onPress={() => { Actions.echoview({ data: 'echoview!' }); }} title='Switch to echoview' /> 
-            <Button onPress={() => { this.props.dispatch(NavigationActions.navigate({ routeName: 'tab_inbox' })); }} title='Switch to tab_inbox' /> 
+            <Button onPress={() => { dispatch(NavigationActions.navigate({ routeName: 'tab_inbox' })) }} title='Switch to tab_inbox' /> 
+            <Button onPress={() => { dispatch({ type: 'Login' }) }} title='dispatch Login' /> 
+            <Button onPress={() => navigate('tab_inbox')} title='Switch to tab_inbox' /> 
 
       </ScrollView>
     )
